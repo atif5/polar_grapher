cd1bd3b6ea9e4258165413370437098049560211
Burzum <atifaydinturanli@gmail.com>
2021-06-30 18:26:39 +0300

    Add files via upload

---
+++ examples/spirals/circle.py
@@ -0,0 +1,7 @@
+
+
+from grapher import draw
+
+
+if __name__ == '__main__':
+	draw('r = 200', continuity=0.002, graph_size=(1000,1000))
---
+++ examples/spirals/tractrix_spiral.py
@@ -0,0 +1,6 @@
+
+from grapher_new import *
+
+
+if __name__ == '__main__':
+	draw_parametric('r = 400*cos(t)', 'theta = tan(t)-t', graph_size=(1000,1000), continuity=0.0001)
