fbe82cb77fe3e4b4e2aecbebe70aeecfa467de09
Burzum <atifaydinturanli@gmail.com>
2021-06-30 20:21:55 +0300

    Update tractrix_spiral.py

--- examples/spirals/tractrix_spiral.py
+++ examples/spirals/tractrix_spiral.py
@@ -3,4 +3,4 @@ from grapher import draw_parametric
 
 
 if __name__ == '__main__':
-	draw_parametric('r = 400*cos(t)', 'theta = tan(t)-t', plane_size=(1000,1000), continuity=0.0001)
+	draw_parametric('r = 400*cos(t)', 't = tan(t)-t', plane_size=(1000,1000), continuity=0.0001)
